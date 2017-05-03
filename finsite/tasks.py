15e13e56cf0e2140cb99c09600ac2a5ab9c3bdd2
Boomerang <boomerang@trafficstars.com>
2017-05-03 11:15:46 +0300

    celery updated

--- finsite/tasks.py
+++ finsite/tasks.py
@@ -1,20 +1,23 @@
 from celery import shared_task
 from celery.task import periodic_task
 from datetime import timedelta
+import decimal
 
 from finsite.models import Currency, CurrencyHistoryRecord
 
 
-@periodic_task(run_every=timedelta(minutes=1))
+@periodic_task(run_every=timedelta(minutes=5))
 def update_prices():
     for c in Currency.objects.all():
         try:
             price = c.data()['price']
-            if abs(c.current_price - price) > 0.0001:
+            if abs(c.current_price - decimal.Decimal(price)) > 0.0001:
                 c.previous_price = c.current_price
                 c.current_price = c.data()['price']
                 c.save()
                 CurrencyHistoryRecord.objects.create(currency=c, price=c.current_price).save()
-                print(c.code, c.current_price, 'updated')            
+                print(c.code, c.current_price, 'updated')
+            else:
+                print('NOT CHANGED', c.code, c.current_price, decimal.Decimal(price))
         except Exception as e:
             print(c.code, e)
