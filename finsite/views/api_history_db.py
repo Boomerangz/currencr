0f22cb8929df4e757f304ff96eba5219959615db
Boomerang <boomerang@trafficstars.com>
2017-05-03 16:48:52 +0300

    Changed filtering in history

--- finsite/views/api_history_db.py
+++ finsite/views/api_history_db.py
@@ -12,23 +12,36 @@ def get_stock_history_from_db(request, code):
             currency = Currency.objects.get(code=code)
         except:
             raise NotFound() 
-
-        date_from = request.GET.get('from', '2016-04-01')
-        date_to = request.GET.get('to', '2016-05-01')
-        date_from = datetime.strptime(date_from, '%Y-%m-%d')
-        date_to = datetime.strptime(date_to, '%Y-%m-%d')
-
+# date =        
         count = request.GET.get('count')
 
-        currency_history_items = list(CurrencyHistoryRecord.objects.\
-            filter(currency=currency))
-        print (currency_history_items)
+        currency_history_items = CurrencyHistoryRecord.objects.\
+            filter(currency=currency).order_by('-time')
 
+        try:
+            date_from = int(request.GET.get('from'))
+            date_from = datetime.fromtimestamp(date_from / 1e3)
+            currency_history_items = currency_history_items.filter(time__gte=date_from)
+        except Exception as e:
+            print(e)
+            currency_history_items = currency_history_items.filter(time__gte=datetime.now()-timedelta(hours=1))
+
+        try:
+            date_to = int(request.GET.get('to'))
+            date_to = datetime.fromtimestamp(date_to / 1e3)
+            currency_history_items = currency_history_items.filter(time__lte=date_to)
+        except Exception as e:
+            print(e)
+
+        print(list(currency_history_items))
+        
         if count is not None:
             try:
                 count = int(count)
             except:
                 raise APIException(detail="Count must be integer")
+
+            currency_history_items = list(currency_history_items)
             if count > 0 and len(currency_history_items) > count:
                 l = len(currency_history_items)
                 c = count
