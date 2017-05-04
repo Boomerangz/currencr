24f8ccf42c37637108239ef78adf1b31741b08a6
Boomerang <boomerang@trafficstars.com>
2017-05-04 09:26:53 +0300

    News improved

--- finsite/views/news.py
+++ finsite/views/news.py
@@ -8,7 +8,7 @@ class NewsView(TemplateView):
     def get_context_data(self, *args, **kwargs):
         context = super(NewsView, self).get_context_data(*args, **kwargs)
         if not self.request.GET.get('url'):
-            feeds = feedparser.parse('http://static.feed.rbc.ru/rbc/logical/footer/news.rss')
+            feeds = feedparser.parse('http://www.finanz.ru/rss/novosti')
             context['news_list'] = [{'title':x['title'], 'link':x['link']} for x in feeds['entries']]
         else:
             url = self.request.GET.get('url')
