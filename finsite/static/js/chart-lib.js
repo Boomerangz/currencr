a12baadaeb77447b3db665ba16bdb6f9784b36e9
Ruslan Akhtyamov <r.akhtyamov@live.com>
2017-05-16 03:52:00 +0300

    Update chart-lib.js

--- finsite/static/js/chart-lib.js
+++ finsite/static/js/chart-lib.js
@@ -1,8 +1,13 @@
+/**
+ * @author R.Akhtyamov
+ * https://github.com/Heterotroph/StreamingChart
+ */
+
 var charts = {};
 
 (function() {
 
-    /**
+    /** 
      * Example of arguments:
      *  size = {width: 1000, height: 400}
      *  point = {width: 50, height: 1}
@@ -10,7 +15,7 @@ var charts = {};
      *  style = {
      *      background: {color: "#000000", alpha: 0.5},
      *      grid: {thickness: 1, color: "#00FFFF", alpha: 1, width: 1, height: 20, dash: [1, 0]},
-     *      zero:  {thickness: 1, color: "#000000", alpha: 1},
+     *      axisX:  {thickness: 1, color: "#000000", alpha: 1, offset: 0},
      *      chart: {
      *          lines: {thickness: 3, color: "#000000", alpha: 0.8, bounds: true},
      *          points:  {thickness: 3, radius: 2, lineColor: "#0000FF", fillColor: "#FF0000", alpha: 0.8, bounds: true}
@@ -32,21 +37,18 @@ var charts = {};
         //dynamic values
         this._widthCapacity = 0;
         this._heightCapacity = 0;
-        this._pointsWidthCapacity = 0;
-        this._calculateCapacity();
-        this._extremeMax = {value: -Number.MAX_VALUE, age: this._widthCapacity};
-        this._extremeMin = {value: Number.MAX_VALUE, age: this._widthCapacity};
-        this._axisOffset = this._axis.offset;
+        this._extremeMax = {value: -Number.MAX_VALUE, age: Number.MAX_VALUE};
+        this._extremeMin = {value: Number.MAX_VALUE, age: Number.MAX_VALUE};
         
         //views
         this._backgroundShape = this.addChild(new createjs.Shape());
         this._gridShape = this.addChild(new createjs.Shape());
-        this._zeroShape = this.addChild(new createjs.Shape());
+        this._axisXShape = this.addChild(new createjs.Shape());
         this._chartShape = this.addChild(new createjs.Shape());
         this._pointShape = this.addChild(new createjs.Shape());
         
         //apply style configuration
-        this.updateStyle();
+        this.redraw();
     }
     
     var p = createjs.extend(StreamingChart, createjs.Container);
@@ -58,7 +60,7 @@ var charts = {};
     p.append = function(data) {
         if (data.length === 0) return;
         var totalData = this._data.concat(data);
-        this._data = totalData.slice(-this._pointsWidthCapacity);
+        this._data = totalData.slice(-this._widthCapacity);
         
         this._searchExtreme(data);
         this._processExtreme();
@@ -72,18 +74,19 @@ var charts = {};
         this._clearChartAndPoints();
         this._extremeMax = {value: -Number.MAX_VALUE, age: this._widthCapacity};
         this._extremeMin = {value: Number.MAX_VALUE, age: this._widthCapacity};
-        this._moveZero();
+        this._moveAxisX(this._style.axisX.offset);
     };
     
     p.setStyle = function(style) {
         this._style = style;
-        this.updateStyle();
+        this.redraw();
     };
     
-    p.updateStyle = function() {
+    p.redraw = function() {
+        this._calculateCapacity();
         this._drawBackgroundShape(this._size, this._style.background);
         this._updateGrid(this._style.grid);
-        this._drawZero(this._style.zero);
+        this._drawAxisX(this._style.axisX);
         this._updateMask(this._style.chart.lines.bounds, this._size.width, this._size.height);
         this.append(this._data.splice(0, this._data.length));
     };
@@ -91,27 +94,92 @@ var charts = {};
     p.setPoint = function(width, height) {
         this._point.width = width;
         this._point.height = height;
-        this._calculateCapacity();
-        this.updateStyle();
     };
     
     p.getPoint = function() {
-        return this._point;
+        return {width: this._point.width, height: this._point.height};
+    };
+    
+    p.setComplexSize = function(width, height) {
+        var widthSegments = this._size.width /  this._point.width;
+        widthSegments = Math.ceil(widthSegments * 1000) / 1000; //TODO: Убрать этот костыль и найти красивое решение
+        var heightSegments = this._size.height / this._point.height;
+        heightSegments = Math.ceil(heightSegments * 1000) / 1000; //TODO: ...
+        this.setSize(width, height);
+        this.setPoint(this._size.width / widthSegments, this._size.height / heightSegments);
     };
     
     p.setSize = function(width, height) {
         this._size.width = width;
         this._size.height = height;
-        this._calculateCapacity();
-        this.updateStyle();
     };
     
     p.getSize = function() {
-        return this._size;
+        return {width: this._size.width, height: this._size.height};
     };
     
-    p.getDisplayData = function() {
-        return _data;
+    p.setGrid = function(width, height) {
+        this._style.grid.width = width;
+        this._style.grid.height = height;
+        this._updateGrid(this._style.grid);
+    };
+    
+    p.getGrid = function() {
+        return {width: this._style.grid.width, height: this._style.grid.height};
+    };
+    
+    p.getData = function() {
+        return this._data.slice();
+    };
+    
+    p.getCapacity = function() {
+        return this._widthCapacity;
+    };
+    
+    p.getInterpolatedValue = function(index) {
+        index = Math.round(index * 100) / 100;
+        index = Math.min(index, this._data.length - 1);
+        index = Math.max(index, 0);
+        var intIndex = Math.floor(index);
+        if (intIndex == index) return this._data[index];
+        var delta = this._data[intIndex + 1] - this._data[intIndex];
+        return this._data[intIndex] + delta * (index - intIndex);
+    };
+    
+    p.getInterpolatedValueByLocalX = function(localX) {
+        var index = this.getIndexByLocalX(localX);
+        return this.getInterpolatedValue(index);
+    };
+    
+    p.getExtreme = function() {
+        var indexCapacity = Math.min(this._data.length, this._widthCapacity) - 1;
+        var min = {
+            value: this._extremeMin.value,
+            index: indexCapacity - this._extremeMin.age
+        };
+        var max = {
+            value: this._extremeMax.value,
+            index: indexCapacity - this._extremeMax.age
+        };
+        return {min: min, max: max};
+    };
+    
+    p.getIndexByLocalX = function(localX) {
+        return localX / this._point.width;
+    };
+    
+    p.getLocalXByIndex = function(index) {
+        var localX = index * this._point.width;
+        return Math.round(localX);
+    };
+    
+    p.getValueByLocalY = function(localY) {
+        return (this._size.height - localY) / this._point.height + this._axis.offset;
+    };
+    
+    p.getLocalYByValue = function(value) {
+        var localY = this._size.height - (this._applyOffset(value) * this._point.height);
+        return Math.round(localY);
     };
     
     //
@@ -124,10 +192,10 @@ var charts = {};
         aY = this._applyOffset(data[0]) * this._point.height;
         this._chartShape.graphics.setStrokeStyle(style.lines.thickness).beginStroke(style.lines.color);
         this._pointShape.graphics.setStrokeStyle(style.points.thickness).beginStroke(style.points.lineColor);
-        if (offsetX === 0) this._drawPoint(0, aY, style.points);
+        if (offsetX === 0) this._drawPoint(0, Math.round(aY), style.points);
         for (var i = 0; i < data.length - 1; i++) {
-            bX = offsetX + stepX * (i + 1);
-            bY = this._applyOffset(data[i + 1]) * this._point.height;
+            bX = Math.round(offsetX + stepX * (i + 1));
+            bY = Math.round(this._applyOffset(data[i + 1]) * this._point.height);
             this._drawSegment(aX, aY, bX, bY, style.lines);
             this._drawPoint(bX, bY, style.points);
             aX = bX;
@@ -175,7 +243,7 @@ var charts = {};
                 graphics.moveTo(x, 0).lineTo(x, this._size.height);
             }
         }
-        var gridOffset = (-this._axisOffset * this._point.height) % stepY;
+        var gridOffset = (-this._axis.offset * this._point.height) % stepY;
         gridOffset = gridOffset < 0 ? gridOffset + stepY : gridOffset;
         if (stepY) {
             for (var y = this._size.height - gridOffset; y >= 0; y -= stepY) {
@@ -206,16 +274,16 @@ var charts = {};
         graphics.moveTo(0, 0).lineTo(this._size.width, 0).endStroke();
     };
     
-    p._drawZero = function(style) {
+    p._drawAxisX = function(style) {
         if (style.alpha === 0) return;
-        this._zeroShape.alpha = style.alpha;
-        this._drawLevelLine(this._zeroShape, style.thickness, style.color);
-        this._moveZero();
+        this._axisXShape.alpha = style.alpha;
+        this._drawLevelLine(this._axisXShape, style.thickness, style.color);
+        this._moveAxisX(style.offset);
     };
     
-    p._moveZero = function() {
-        this._zeroShape.y = this._size.height - this._applyOffset(0) * this._point.height;
-        this._zeroShape.visible = this._isInsideBounds(0, this._zeroShape.y);
+    p._moveAxisX = function(offset) {
+        this._axisXShape.y = this._size.height - this._applyOffset(offset) * this._point.height;
+        this._axisXShape.visible = this._isInsideBounds(0, this._axisXShape.y);
     };
     
     p._processExtreme = function() {
@@ -224,7 +292,7 @@ var charts = {};
         var isPointHeightChanged = this._calculatePointHeight();
         if (!isAxisOffsetChanged && !isPointHeightChanged) return;
         this._updateGrid(this._style.grid);
-        this._moveZero();
+        this._moveAxisX(this._style.axisX.offset);
     };
     
     p._clearChartAndPoints = function() {
@@ -237,30 +305,29 @@ var charts = {};
     //
     
     p._calculateAxisOffset = function() {
-        var tempOffset = this._extremeMin.value - this._axis.dynamicSpace.bottom;
-        var result = this._axisOffset != tempOffset;
-        this._axisOffset = tempOffset;
+        var tempOffset = this._extremeMin.value - this._axis.dynamicSpace.bottom / this._point.height;
+        var result = this._axis.offset != tempOffset;
+        this._axis.offset = tempOffset;
         return result;
     };
     
     p._calculatePointHeight = function() {
         var newPointHeight = this._point.height;
-        newPointHeight = this._size.height / this._applyOffset(this._extremeMax.value + this._axis.dynamicSpace.top);
+        newPointHeight = this._size.height / this._applyOffset(this._extremeMax.value + this._axis.dynamicSpace.top / this._point.height);
         newPointHeight = Math.min(newPointHeight, this._size.height);
-        this._heightCapacity = Math.floor(this._size.height / newPointHeight);
         var result = this._point.height != newPointHeight;
         this._point.height = newPointHeight;
+        this._calculateCapacity();
         return result;
     };
     
     p._calculateCapacity = function() {
-        this._widthCapacity = Math.floor(this._size.width /  this._point.width);
-        this._heightCapacity = Math.floor(this._size.height / this._point.height);
-        this._pointsWidthCapacity = this._widthCapacity + 1;
+        this._widthCapacity = Math.floor(this._size.width /  this._point.width) + 1;
+        this._heightCapacity = Math.floor(this._size.height / this._point.height) + 1;
     };
     
     p._applyOffset = function(y) {
-        return y - this._axisOffset;
+        return y - this._axis.offset;
     };
     
     p._isInsideBounds = function(x, y) {
@@ -272,27 +339,27 @@ var charts = {};
         var newExtreme;
         this._extremeMax.age += data.length;
         this._extremeMin.age += data.length;
-        var isOld = this._extremeMax.age >= this._pointsWidthCapacity;
-        isOld = isOld || this._extremeMin.age >= this._pointsWidthCapacity;
+        var isOld = this._extremeMax.age >= this._widthCapacity;
+        isOld = isOld || this._extremeMin.age >= this._widthCapacity;
         if (isOld || !data.length) {
             newExtreme = this._getExtreme(Number.MAX_VALUE, -Number.MAX_VALUE, this._data);
         } else {
             newExtreme = this._getExtreme(this._extremeMin.value, this._extremeMax.value, data);
         }
-        this._extremeMin = newExtreme[0];
-        this._extremeMax = newExtreme[1];
+        this._extremeMin = newExtreme.min;
+        this._extremeMax = newExtreme.max;
     };
     
     p._getExtreme = function(min, max, data) {
-        var currentMax = {value: max, age: this._pointsWidthCapacity};
-        var currentMin = {value: min, age: this._pointsWidthCapacity};
+        var currentMax = {value: max, age: this._widthCapacity};
+        var currentMin = {value: min, age: this._widthCapacity};
         for (var i = 0; i < data.length; i++) {
             currentMax.value = Math.max(currentMax.value, data[i]);
             if (currentMax.value == data[i]) currentMax.age = data.length - i - 1;
             currentMin.value = Math.min(currentMin.value, data[i]);
             if (currentMin.value == data[i]) currentMin.age = data.length - i - 1;
         }
-        return [currentMin, currentMax];
+        return {min: currentMin, max: currentMax};
     };
     
     //  ---
