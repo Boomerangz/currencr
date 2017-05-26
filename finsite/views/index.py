6dd43e2241b631c63800f539d20db0a3b9ab3c1b
Boomerang <boomerang@trafficstars.com>
2017-05-26 14:04:44 +0300

    Added search in index page

--- finsite/views/index.py
+++ finsite/views/index.py
@@ -1,3 +1,5 @@
+import feedparser
+from django.db.models import Q
 from django.views.generic import TemplateView
 
 from finsite.models import Currency
@@ -8,5 +10,16 @@ class IndexView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super(IndexView, self).get_context_data(**kwargs)
+        search = self.request.GET.get('search')
         context['currency_list'] = Currency.objects.all().order_by('id')
+        if search:
+            context['currency_list'] = context['currency_list']\
+                .filter(Q(name__icontains=search)|Q(code__icontains=search))
+        context['news_list'] = self.get_news()
         return context
+
+    def get_news(self):
+        feeds_list = ['http://www.finanz.ru/rss/novosti']
+        feeds = [feedparser.parse(f) for f in feeds_list]
+        return sum([[{'title': x['title'], 'link': x['link'], 'date': x['published']} \
+                                     for x in f['entries']] for f in feeds], [])
