06f56236c1d034f36d0b36a320aee3c10c958a0c
Boomerang <boomerang@trafficstars.com>
2017-05-26 14:04:25 +0300

    added news

--- finsite/views/currency.py
+++ finsite/views/currency.py
@@ -1,3 +1,4 @@
+import feedparser
 from django.views.generic import TemplateView
 
 from finsite.models import Currency
@@ -8,5 +9,12 @@ class CurrencyView(TemplateView):
     def get_context_data(self, code, **kwargs):
         context = super(CurrencyView, self).get_context_data(**kwargs)
         context['currency'] = Currency.objects.get(code__iexact=code)
-
+        context['news_list'] = self.get_news()
         return context
+
+
+    def get_news(self):
+        feeds_list = ['http://www.finanz.ru/rss/novosti']
+        feeds = [feedparser.parse(f) for f in feeds_list]
+        return sum([[{'title': x['title'], 'link': x['link'], 'date': x['published']} \
+                                     for x in f['entries']] for f in feeds], [])
