661b4f9fd7c3cee78fa8b2721dbb718bf8559109
Boomerang <boomerang@trafficstars.com>
2017-07-14 12:19:57 +0300

    better index ordering

--- finsite/views/index.py
+++ finsite/views/index.py
@@ -12,7 +12,7 @@ class IndexView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super(IndexView, self).get_context_data(**kwargs)
         search = self.request.GET.get('search')
-        context['currency_list'] = Currency.objects.all().order_by('ordering')
+        context['currency_list'] = Currency.objects.all().order_by('ordering', 'id')
         if search:
             context['currency_list'] = context['currency_list']\
                 .filter(Q(name__icontains=search)|Q(code__icontains=search))
