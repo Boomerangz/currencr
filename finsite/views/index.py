a9a32a94ef53b240d05c51cfc946fcc43700da7e
Boomerang <boomerang@trafficstars.com>
2017-07-22 14:30:30 +0300

    fixed 24 hours stats

--- finsite/views/index.py
+++ finsite/views/index.py
@@ -1,3 +1,5 @@
+from datetime import datetime, timedelta
+
 import feedparser
 from django.db.models import Q, Avg, Sum
 from django.views.generic import TemplateView
@@ -19,15 +21,18 @@ class IndexView(TemplateView):
                 .filter(Q(name__icontains=search)|Q(code__icontains=search))
 
         currency_list = list(currency_list)
+        day_ago = datetime.now() - timedelta(hours=24)
         for c in currency_list:
-            c.day_history = self.get_history_for_currency(c)
+            c.day_history = self.get_history_for_currency(c, from_time=day_ago)
         context['news_list'] = get_news(search=search, limit=len(currency_list), language=translation.get_language())
         context['currency_list'] = currency_list
         return context
 
-    def get_history_for_currency(self, currency):
+    def get_history_for_currency(self, currency, from_time=None):
+        if not from_time:
+            from_time = datetime.now() - timedelta(hours=24)
         return list(map(lambda c: c['price'], CurrencyHistoryRecord.objects.\
-                filter(currency=currency).order_by('time') \
+                filter(currency=currency, time__gte=from_time).order_by('time') \
                                 .extra(select= {'time':"date_trunc('hour', time)"}) \
                                 .values("time") \
                                 .annotate(price=Avg('price'), volume=Sum('volume'))))
