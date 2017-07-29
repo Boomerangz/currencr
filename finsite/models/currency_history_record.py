c994ad3a5f24153c57c3af7653c49ce00a17a18c
Your Name <you@example.com>
2017-07-29 10:49:25 +0300

    changed history model

--- finsite/models/currency_history_record.py
+++ finsite/models/currency_history_record.py
@@ -4,5 +4,5 @@ from .currency import Currency
 class CurrencyHistoryRecord(models.Model):
     currency = models.ForeignKey(Currency)
     price = models.DecimalField(decimal_places=5,max_digits=15, default=0)
-    time = models.DateTimeField(auto_now_add=True)
+    time = models.DateTimeField()
     volume = models.FloatField(blank=True, null=True, default=None)
