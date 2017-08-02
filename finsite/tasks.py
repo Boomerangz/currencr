e5e2cc8cfd5157349bdaa039c12bd96b19fddbd0
Boomerang <boomerang@trafficstars.com>
2017-08-02 10:47:55 +0300

    added grabbing of forklog

--- finsite/tasks.py
+++ finsite/tasks.py
@@ -5,6 +5,7 @@ from datetime import timedelta, datetime
 import decimal
 import requests
 import pytz
+from django.db.models import Q
 
 from newspaper import Article
 
@@ -47,18 +48,19 @@ def update_prices(exchange_name="Kraken", update_currency=True):
             if days_ago is not None:
                 curr.current_price = last_history.price
                 curr.previous_price = days_ago.price
-                print(curr.code, curr.current_price, curr.previous_price)
             curr.save()
 
 
 @periodic_task(run_every=timedelta(minutes=5))
 def update_news_ru():
     feeds_list = ['https://bitnovosti.com/feed/']
-    print(feeds_list)
     feeds = [feedparser.parse(f) for f in feeds_list]
     news_list =  sum([[{'title': x['title'], 'link': x['link'], 'date': x['published']} \
                  for x in reversed(f['entries'])] for f in feeds], [])
     news_list.extend(map(lambda x: {'link':x}, reversed(list(get_news_from_bitmedia()))))
+    news_list.extend(map(lambda x: {'link':x}, reversed(list(get_news_from_forklog()))))
+    existing_links = set(NewsItem.objects.filter(link__in=[n['link'] for n in news_list]).values_list('link', flat=True))
+    news_list = [n for n in news_list if n['link'] not in existing_links]
     for news in news_list:
         try:
             article = Article(news['link'], language='ru')
@@ -73,16 +75,14 @@ def update_news_ru():
                 text = text[:text.find('Материал предоставил')]
             text = text.replace('on •<br/><br/>','')
             text = text.replace('<br/>Источник<br/>','')
-            summary = article.summary
             top_image = article.top_image
             if top_image == 'http://www.finanz.ru/Images/FacebookIcon.jpg':
                 top_image = None
             keywords = article.keywords
-            if NewsItem.objects.filter(Q(title__iexact=title)|Q(link__iexact=news['link'])).count() == 0:
+            if NewsItem.objects.filter(title__iexact=title).count() == 0:
                 NewsItem.objects.create(link=news['link'],
                                         title=title,
                                         text=text,
-                                        summary=summary,
                                         image=top_image,
                                         keywords=keywords,
                                         language='ru')
@@ -92,7 +92,6 @@ def update_news_ru():
 @periodic_task(run_every=timedelta(minutes=5))
 def update_news_en():
     feeds_list = ['https://cryptoinsider.com/feed/']
-    print(feeds_list)
     feeds = [feedparser.parse(f) for f in feeds_list]
     news_list =  sum([[{'title': x['title'], 'link': x['link'], 'date': x['published']} \
                  for x in f['entries']] for f in feeds], [])
@@ -106,7 +105,6 @@ def update_news_en():
             if 'ТАСС:' in title:
                 continue
             text = '<br/>'.join([s for s in article.text.split('\n') if 'Categories:' not in s and 'Tags:' not in s]).strip()
-            summary = article.summary
             top_image = article.top_image
             if top_image == 'http://www.finanz.ru/Images/FacebookIcon.jpg':
                 top_image = None
@@ -115,7 +113,6 @@ def update_news_en():
                 NewsItem.objects.create(link=news['link'],
                                         title=title,
                                         text=text,
-                                        summary=summary,
                                         image=top_image,
                                         keywords=keywords,
                                         language='en')
@@ -139,3 +136,22 @@ def get_news_from_bitmedia():
         if i>=20:
             break
         yield "https://bits.media%s" % m.group(0).replace('<a href="', '').replace('">', '')
+
+
+def get_news_from_forklog():
+    import requests
+    import re
+    from bs4 import BeautifulSoup
+    link = 'http://forklog.com/news/'
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.9; rv:45.0) Gecko/20100101 Firefox/45.0'
+    }
+    r = requests.get(link, headers = headers)
+    text = r.text
+    soup = BeautifulSoup(text, 'html.parser')
+    article_list = soup.findAll('article', {'class':'post'})
+    for article in article_list:
+        link = article.find('a')
+        if not link:
+                continue
+        yield link['href']
