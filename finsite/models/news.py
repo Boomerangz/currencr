e0d3cf3748e4b2493c545acf14ef0a8987f56150
Boomerang <boomerang@trafficstars.com>
2017-08-04 10:01:59 +0300

    changed and cached summary

--- finsite/models/news.py
+++ finsite/models/news.py
@@ -1,6 +1,10 @@
 from datetime import datetime
 from django.db import models
 from django.contrib.postgres.fields import ArrayField
+from cacheops import cached
+
+from finsite import models as fin_models
+
 
 class NewsItem(models.Model):
     title = models.CharField(max_length=500, null=False, blank=False)
@@ -16,11 +20,24 @@ class NewsItem(models.Model):
     def __str__(self):
         return "%d %s %s"%(self.id, self.title, datetime.strftime(self.created_at, "%Y-%m-%d %H:%M"))
 
+    @cached(timeout=3600*12)
     def summary(self):
-        summary = self.text.replace('<br>','\n').replace('<br/>','\n').strip().split('\n')[0]
-        if len(summary) > 200:
-            summary = ' '.join(summary[:197].split(' ')[:-1]) + '...'
+        summary = self.text.replace('<br>','\n').replace('<br/>','\n').strip()
+        summary = '\n'.join([s.strip() for s in summary.split('\n')])
+        summary_len = 0
+        while summary_len != len(summary):
+            summary_len = len(summary)
+            summary = summary.replace('\n\n', '\n')
+            summary = summary.replace('  ', ' ')
+        summary = summary[:600]
+        if '\n' in summary:
+            summary = '\n'.join(summary.split('\n')[1:])
+        if '.' in summary:
+            summary = '.'.join(summary.split('.')[:-1])+'\n...'
+        else:
+            summary = ' '.join(summary.split(' ')[:-1])+'...'
         return summary
+
 
 
 class KeywordSynonims(models.Model):
