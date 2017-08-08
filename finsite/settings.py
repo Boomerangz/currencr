363183d2acb06c8cb395cd2846f4ba4d943204d5
Boomerang <boomerang@trafficstars.com>
2017-08-08 18:14:57 +0300

    added translation and chinese

--- finsite/settings.py
+++ finsite/settings.py
@@ -122,11 +122,12 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
-# LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'en-us'
 
 LANGUAGES = [
     ('en','English'),
     ('ru','Russian'),
+    ('zh','Chinese simplified'),
 ]
 
 LOCALEURL_USE_ACCEPT_LANGUAGE = True
