41b755fc804b78459db78d6de610936576104849
root <you@example.com>
2019-07-20 04:55:34 +0000

    -yahoo

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,9 @@
 FROM python:3.6
 WORKDIR /usr/src/app
 COPY requirements.txt ./
-RUN pip install --no-cache-dir -r requirements.txt
+RUN pip install -r requirements.txt
 COPY . .
+EXPOSE 8000
+RUN python manage.py collectstatic 
 CMD ls && bash run_server.sh
 
--- run_server.sh
+++ run_server.sh
@@ -1,1 +1,2 @@
-gunicorn finsite.wsgi application -b 0.0.0.0:8000 -w 1 --timeout=60 --graceful-timeout=60 --max-requests=1024
+python ./manage.py runserver 0.0.0.0:8000
+#gunicorn finsite.wsgi application -b 0.0.0.0:8000 -w 1 --timeout=60 --graceful-timeout=60 --max-requests=1024
